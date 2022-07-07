f7f3866dd2a7b593f565638d11906685e44a281e
Eduard Marrugo <eduarmarrugocm@unimagdalena.edu.co>
2022-07-06 19:13:15 -0500

    Update

--- js/pokeAPI.js
+++ js/pokeAPI.js
@@ -1,17 +1,36 @@
-
-$.ajax({
-  method: "GET",
-  url: "https://pokeapRi.co/api/v2/pokemon/ditto",
-})
-  .done(function (data) {
-    console.log(data) // imprimimos la respuesta
+document.getElementById("pokeAPI").addEventListener("click", function () {
+  $.ajax({
+    method: "GET",
+    url: "https://pokeapRi.co/api/v2/pokemon/ditto",
   })
-  .fail(function () {
-    Swal.fire({
-      title: "Error!",
-      text: "Request Error",
-      icon: "error",
-      toast: true,
-      position: "top-end"
+    .done(function (data) {
+      console.log(data); // imprimimos la respuesta
+    })
+    .fail(function () {
+      Swal.fire({
+        title: "Error!",
+        text: "Request Error",
+        icon: "error",
+        toast: true,
+        position: "top-end",
+      });
     });
-  });
+});
+// $("#pokeAPI").on("click", function () {
+//   $.ajax({
+//     method: "GET",
+//     url: "https://pokeapRi.co/api/v2/pokemon/ditto",
+//   })
+//     .done(function (data) {
+//       console.log(data) // imprimimos la respuesta
+//     })
+//     .fail(function () {
+//       Swal.fire({
+//         title: "Error!",
+//         text: "Request Error",
+//         icon: "error",
+//         toast: true,
+//         position: "top-end"
+//       });
+//     });
+// });
